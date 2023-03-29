c00832adcc1b6ff98617d81cbe9bbdb3034e6a14
qkrp0215 <qkrp0215@naver.com>
2023-03-29 14:39:35 +0900

    pc 커밋 2

--- main.java
+++ main.java
@@ -4,4 +4,10 @@ int num = 10;
 System.out.printin(num);
 
 System.out.prinin("오늘 뭐먹을까");
-  
+// 배열 생성하기
+// 배열 생성할 때 배열 크기 정해주기  
+int[] arr1 = new int[5];
+arr1 = {1,2,3,4,5,6,};
+
+
+System.out.prinin(arr1);
